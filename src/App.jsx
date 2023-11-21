c2f950ecdf8173d476f961d458eff9f77190a4b6
Sidra Khan <sidrakhan.me.100@gmail.com>
2023-11-21 18:59:47 +0500

    📦NEW: Initial Work

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
+import { CORE_CONCEPTS } from "./data";
+
+function App() {
+  function handleSelect() {
+    console.log("Hello World - selected!");
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+          Dynamic Content
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import conceptsImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const title = reactDescriptions[getRandomNumber(2)];
+  return (
+    <header>
+      <img src={conceptsImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {title} React concepts you will need for almost any app you are going to
+        build!
+      </p>
+    </header>
+  );
+}
